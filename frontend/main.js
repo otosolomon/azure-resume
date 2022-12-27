c836c1ab270a4f5925400caabc26f6cf3a719ccf
Oto <oto.solomon@hotmail.com>
2022-12-27 14:41:19 +0000

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterapp.azurewebsites.net/api/GetResumeCounter?code=TZraAiYNSvqxG-VZZTOblL1XtGN23KFpWXf74pbTBEeLAzFudbxjFQ=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(Response => {
+    fetch(functionApiUrl).then(Response => {
         return Response.json()
     }).then(response => {
         console.log("website called function API.");
